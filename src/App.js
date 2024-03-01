3d9186f3f3f9968850bd62b70bf73df96eef11f7
satvikibudhia <122771370+satvikibudhia@users.noreply.github.com>
2024-03-01 23:51:35 +0530

    1st page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import Lottie from 'lottie-react';
 import animationData from './ani.json';
-import './App.css'; // Import your CSS file
+import './App.css';
 import herobg from './images/herobg.png';
-import pic from './images/pic.png'; // Assuming you have an image named pic for smaller screens
+import pic from './images/pic.png';
+
+window.addEventListener('orientationchange', function() {
+  setTimeout(function(){
+      window.location.reload();
+  }, 300);
+});
 
 const App = () => {
   const [dimensions, setDimensions] = useState({ width: window.innerWidth, height: window.innerHeight });
@@ -11,31 +17,31 @@ const App = () => {
   const [showExpandedText, setShowExpandedText] = useState(false);
 
   useEffect(() => {
+    document.body.style.overflow = 'hidden';
+
     const handleResize = () => {
       setDimensions({ width: window.innerWidth, height: window.innerHeight });
     };
-    window.addEventListener('resize', handleResize);
-    return () => {
-      window.removeEventListener('resize', handleResize);
+
+    const handleOrientationChange = () => {
+      setTimeout(handleResize, 300);
     };
-  }, []);
 
-  useEffect(() => {
-    // Prevent scrolling
-    document.body.style.overflow = 'hidden';
+    handleResize();
+
+    window.addEventListener('resize', handleResize);
+    window.addEventListener('orientationchange', handleOrientationChange);
 
     return () => {
-      // Re-enable scrolling when the component is unmounted
       document.body.style.overflow = 'auto';
+      window.removeEventListener('resize', handleResize);
+      window.removeEventListener('orientationchange', handleOrientationChange);
     };
   }, []);
 
   const handleAnimationComplete = () => {
-    // Animation has finished playing, show the text
     setShowText(true);
-    // Enable scrolling downward
-    document.body.style.overflowY = 'scroll';
-    // After a delay, show expanded text
+    document.body.style.overflowY = 'hidden';
     setTimeout(() => setShowExpandedText(true), 1000);
   };
 
@@ -43,10 +49,11 @@ const App = () => {
     <div style={{ 
       width: dimensions.width, 
       height: dimensions.height, 
-      backgroundImage: dimensions.width < 700 ? `linear-gradient(rgba(255, 255, 255, 0.5), rgba(255, 255, 255, 0.5)),url(${pic})` : `url(${herobg})`, 
-      backgroundSize: 'cover', // Adjust as needed
-      backgroundPosition: 'center', // Adjust as needed
-      backgroundRepeat: 'no-repeat' // Prevent repeating the background image
+      backgroundImage: dimensions.width < 700 ? `url(${pic})` : `url(${herobg})`
+      , 
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+      backgroundRepeat: 'no-repeat'
     }}>
       <Lottie 
         animationData={animationData} 
@@ -56,24 +63,36 @@ const App = () => {
         onComplete={handleAnimationComplete} 
       />
       {!showExpandedText && (
-        <div className="text-container">
+        <div className="text-container" style={{ textAlign: 'center' }}>
         </div>
       )}
       {showText && (
-        <div className="text-container">
-          <div className={showExpandedText ? "expanded-text" : "gdsc-text"}>
+        <div className="text-container" style={{ textAlign: 'left', paddingTop: dimensions.width < 700 ? '38.5vh' : '5vw' }}>
+          <div className={showExpandedText ? "expanded-text" : "gdsc-text"} style={{ lineHeight: "1.2", paddingLeft: dimensions.width < 700 ? "30.5vw" : "5vw" }}>
             {showExpandedText ? (
               <>
-                <span className="red">G</span>
-                oogle <span className="blue">D</span>evelopers <span className="green">S</span>tudent <span className="yellow">C</span>lub
+                <span className="red">G</span>oogle <span className="blue">D</span>evelopers <span className="green">S</span>tudent <span className="yellow">C</span>lub
+                <br />
+                <span className="blue" style={{ fontSize: dimensions.width < 700 ? "4.3vw" : "1.6vw", align:'center' }}>
+                  Symbiosis Institute of Technology Chapter
+                </span>
               </>
             ) : (
-              <>
+              <div style={{ 
+                textAlign: 'center', 
+                paddingRight: window.innerWidth < 700 ? "20.5vw" : "5vw",
+                paddingBottom: window.innerWidth < 700 ? "30.5vw" : "5vw"
+              }}>
                 <span className="red">G</span>
                 <span className="blue">D</span>
                 <span className="green">S</span>
-                <span className="yellow">C</span>
-              </>
+                <span className="yellow">C</span><br/>
+                <span className="blue" style={{fontSize: window.innerWidth < 700 ? "4.5vw" : "1.2vw"}} >
+  Symbiosis Institute of Technology Chapter
+</span>
+<br />
+
+              </div>
             )}
           </div>
         </div>
@@ -81,5 +100,4 @@ const App = () => {
     </div>
   );
 }
-
 export default App;
